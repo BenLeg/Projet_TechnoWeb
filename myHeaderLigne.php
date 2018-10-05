183ed4344c0055d05b97516b9972fe98edd6c64b
noemieblt <noemieboillot5@hotmail.fr>
2018-10-05 14:30:02 +0200

    HeaderLigne
    Hedear du site

---
+++ myHeaderLigne.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html>
+<head><link rel="stylesheet" href="style.css" /></head>
+
+<header>
+	<link rel="stylesheet" href="css/common.css" />
+
+<div id="conteneur1">
+	<div class="element1"> <a id="nomdusite" href="test.php">MonSiteWeb</a> </div>
+
+	<div class="element2">	
+		<form id="connexion" action='test.php' method='get'>
+		<input class="input" type='text' name='myName' value='Identifiant' /><br />
+		<input class="input" type='text' name='mypassword' value='Mot de passe'/><br />
+		<input type='submit' >
+		</form>
+
+
+
+	</div>
+</div>
+
+<div id="conteneur2">
+
+    <div class="element2"><input type="text" placeholder="Search.."></div>
+
+    <div class="element2"> <a id="produits" href="produits.php">LesProduits</a> </div>
+
+    <div class="element2"> <a id="panier" href="panier.php">MonPanier</a> </div>
+
+</div>
+	
+
+</header>
+
+</html>
