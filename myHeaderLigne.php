a1c4b99fce3f748ab50dd9f9d358381b3a5c6663
noemieblt <noemieboillot5@hotmail.fr>
2018-10-05 16:33:04 +0200

    no

--- myHeaderLigne.php
+++ myHeaderLigne.php
@@ -3,11 +3,14 @@
 <html>
 <head><link rel="stylesheet" href="style.css" /></head>
 
+
 <header>
-	<link rel="stylesheet" href="css/common.css" />
+	<link rel="stylesheet" href="mcss/common.css" />
 
 <div id="conteneur1">
-	<div class="element1"> <a id="nomdusite" href="test.php">MonSiteWeb</a> </div>
+<div class='element3'>	<img src="images/macaps.png" width="50px">	</div>
+
+	<div class="element1">  <a id="nomdusite" href="test.php">SiteWeb</a> </div>
 
 	<div class="element2">	
 		<form id="connexion" action='test.php' method='get'>
@@ -23,11 +26,10 @@
 
 <div id="conteneur2">
 
-    <div class="element2"><input type="text" placeholder="Search.."></div>
+    <div class="elem1"><input type="text" placeholder="Search.."></div>
 
-    <div class="element2"> <a id="produits" href="produits.php">LesProduits</a> </div>
-
-    <div class="element2"> <a id="panier" href="panier.php">MonPanier</a> </div>
+    <div class="elem2"> <a id="produits" href="produits.php">LesProduits</a> </div>
+    <div class="elem3"> <a id="panier" href="panier.php">MonPanier</a> </div>
 
 </div>
 	
