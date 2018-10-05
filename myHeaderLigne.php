4d9f13288cdb07f3c0d01d56876c1f6a27547ab1
noemieblt <noemieboillot5@hotmail.fr>
2018-10-05 17:37:43 +0200

    LastCont

--- myHeaderLigne.php
+++ myHeaderLigne.php
@@ -7,15 +7,17 @@
 <header>
 	<link rel="stylesheet" href="mcss/common.css" />
 
+
 <div id="conteneur1">
+
 <div class='element3'>	<img src="images/macaps.png" width="50px">	</div>
 
-	<div class="element1">  <a id="nomdusite" href="test.php">SiteWeb</a> </div>
+	<div class="element1">  <a id="nomdusite" href="test.php">Ma Capsule</a> </div>
 
 	<div class="element2">	
 		<form id="connexion" action='test.php' method='get'>
-		<input class="input" type='text' name='myName' value='Identifiant' /><br />
-		<input class="input" type='text' name='mypassword' value='Mot de passe'/><br />
+		<input type='text' value='Identifiant' /> 
+		<input type='text' value='Mot de passe'/> 
 		<input type='submit' >
 		</form>
 
@@ -25,10 +27,9 @@
 </div>
 
 <div id="conteneur2">
-
-    <div class="elem1"><input type="text" placeholder="Search.."></div>
-
-    <div class="elem2"> <a id="produits" href="produits.php">LesProduits</a> </div>
+    
+    <div class="elem1"> <a id="produits" href="produits.php">LesProduits</a> </div>
+    <div class="elem2"><input type="text" href="search.php" value='Search'></div>
     <div class="elem3"> <a id="panier" href="panier.php">MonPanier</a> </div>
 
 </div>
