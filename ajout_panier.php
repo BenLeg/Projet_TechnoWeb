b8af25c91d5134444dbf4c6dc3071a82cff9a5cf
SamLef <43677207+SamLef@users.noreply.github.com>
2018-10-12 14:34:30 +0200

    init dynamic V1

---
+++ ajout_panier.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+
+<?php
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=capsules;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+?>
+
+<html>
+	<head>
+		<title>Capsules.com</title>
+		<link rel="stylesheet" href="product_css.css" />
+	</head>
+
+	<header>
+        <h1>P'ISENLOVE</h1>  
+    </header>
+    <body>
+        <?php
+            $id = $_GET['id'];
+            /*$user = $_GET['user'];*/
+            $quantity = $_POST['quantity'];
+
+            /*$req = $bdd->prepare('INSERT INTO jeux_video(nom, possesseur, console, prix, nbre_joueurs_max, commentaires) VALUES(:nom, :possesseur, :console, :prix, :nbre_joueurs_max, :commentaires)');
+            $req->execute(array(
+                'nom' => $nom,
+                'possesseur' => $possesseur,
+                'console' => $console,
+                'prix' => $prix,
+                'nbre_joueurs_max' => $nbre_joueurs_max,
+                'commentaires' => $commentaires
+                ));*/
+        ?>
+        <section>
+            <div id="cadre_princ">
+                <div class="ajout">
+                    <?php echo $quantity ?>
+                    <h> Article(s) Ajouté(s) au panier <br></h>
+                </div>
+                <a href="search_p.php">
+                    <div class="keep_shopping">
+                        <h>Keep Shopping</h>
+                    </div>
+                </a>
+                <a href="cart.php">
+                    <div class="check_cart">
+                        <h>Check Cart</h>
+                    </div>
+                </a>
+            </div>
+        </section>
+    </body>
+</html>
