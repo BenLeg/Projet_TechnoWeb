c8356ef7fcc55ea0fc33aed72a92bb61d3782aae
noemieblt <noemieboillot5@hotmail.fr>
2018-10-12 14:29:49 +0200

    Update myHeaderLigne.php

--- myHeaderLigne.php
+++ myHeaderLigne.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 
+
+
+
 <html>
 <head><link rel="stylesheet" href="style.css" /></head>
 
@@ -8,20 +11,62 @@
 	<link rel="stylesheet" href="mcss/common.css" />
 
 
+
+
+
+<?php
+
+	$bdd = new PDO('mysql:host=localhost;dbname=project;charset=utf8', 'root', '');
+	
+	try	{			$bdd = new PDO('mysql:host=localhost;dbname=project;charset=utf8', 'root', '');		}
+	catch (Exception $e)	{	        die('Erreur : ' . $e->getMessage());		}
+	
+	$user_id = '';
+
+    if (	isset($_POST['identifiant']) AND isset($_POST['mdp'])  ){
+    	$identifiant = $_POST['identifiant'] ;
+    	$mdp = $_POST['mdp'] ;
+    	$result = $bdd->query('SELECT * FROM users');
+    		$donnee = $result->fetch();
+    	while($donnee != null){
+    		if($donnee['username']==$identifiant AND $donnee['password']==$mdp){
+    			$user_id = $donnee['id'];
+
+    		}
+    		$donnee = $result->fetch();
+    	} 
+    	$result->closeCursor();
+
+
+    }
+
+    else  {    echo '<p>pas didentifiant</p>';  }
+
+
+
+    ?>
+
+
+
 <div id="conteneur1">
 
 <div class='element3'>	<img src="images/macaps.png" width="50px">	</div>
 
 	<div class="element1">  <a id="nomdusite" href="test.php">Ma Capsule</a> </div>
-
-	<div class="element2">	
-		<form id="connexion" action='test.php' method='get'>
-		<input type='text' value='Identifiant' /> 
-		<input type='text' value='Mot de passe'/> 
+	<div class="element2" id="deco">
+		<form id="connexion" action='myHeaderLigne2.<?php  ?>' method='post'>
+		Username : <input type='text' name='identifiant' /> 
 		<input type='submit' >
+		</br>
+		Password : <input type='text' name='mdp'' /> 
 		</form>
+	</div>
 
-
+	<div class="element2" id="co">	
+	</br>
+		Bonjour <?php echo($identifiant)?>
+		Vous êtes connecté.
+		</form>
 
 	</div>
 </div>
