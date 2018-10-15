e3e942def958b1f9639e9d2765aa542880b7ad44
noemieblt <noemieboillot5@hotmail.fr>
2018-10-15 17:02:18 +0200

    style

--- myHeaderLigne.php
+++ myHeaderLigne.php
@@ -26,7 +26,7 @@
     	while($donnee != null){
     		if($donnee['username']==$identifiant AND $donnee['password']==$mdp){
     			$user_id = $donnee['id'];
-#    			echo 'vous etes co';
+    			echo 'vous etes co';
 				$co=true;
     		}
     		$donnee = $result->fetch();
@@ -51,26 +51,27 @@
 
 <div id="conteneur1">
 
-<div class='element3'>	<img src="images/macaps.png" width="50px">	</div>
+	<div class="element3">	<img src="images/macaps.png" width="50px">	</div>
 
 	<div class="element1">  <a id="nomdusite" href="myHeaderLigne.php">Ma Capsule</a> </div>
-	<div class="element2" id="deco">
-		<form id="connexion"  method='post'> 
-		Username : <input type='text' name='identifiant' />
-		<input type='Submit' value='Envoyer'>
+	<div class="element2" >
+		<form id="connexion" method="post"> 
+		Username : <input type="text" name="identifiant" />
+		<input type="submit" value="Envoyer" />
 		</br>
-		Password : <input type='text' name='mdp'/> 
+		Password : <input type="text" name="mdp"/> 
 		</form> 
 	</div>
 
-	<div class="element2" id="co">	
-	</br>
+	<div class="element2">	
+		<div id="co">
+		</br>
 		Bonjour <?php echo $identifiant  ?>
-		Vous êtes connecté.
-		</form>
+		Vous êtes connecté.</div>
 	</div>
 
 </div>
+
 <div id="conteneur2">
     
     <div class="elem1"> <a id="produits" href="produits.php">LesProduits</a> </div>
