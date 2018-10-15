06ee53e6d2d4b2d98bc5b73733cec9698f022944
noemieblt <noemieboillot5@hotmail.fr>
2018-10-15 16:24:08 +0200

    styl

--- myHeaderLigne.php
+++ myHeaderLigne.php
@@ -12,7 +12,7 @@
 
 <?php
 
-	$bdd = new PDO('mysql:host=localhost;dbname=project;charset=utf8', 'root', '');
+	$bdd = new PDO('mysql:host=localhost;dbname=capsules;charset=utf8', 'root', '');
 	
 	try	{			$bdd = new PDO('mysql:host=localhost;dbname=project;charset=utf8', 'root', '');		}
 	catch (Exception $e)	{	        die('Erreur : ' . $e->getMessage());		}
