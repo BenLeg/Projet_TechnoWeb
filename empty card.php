fab402bcd6c9691f8a340096dfd2ed2666207ff2
SamLef <43677207+SamLef@users.noreply.github.com>
2018-10-15 18:33:54 +0200

    modif_commit_lolo
    header inclus
    mise en commun des css

--- empty card.php
+++ empty card.php
@@ -1,58 +1,44 @@
-<?php 
-	try{
-		$bdd = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root', '');
-	}
-	catch(Exception $e){
-		die('erreur:' .$e->getMessage());
-	}
-
- ?>
 <!DOCTYPE html>
-<?php
-$UsID=1;/*$_GET['user'];*/
-$request="SELECT * FROM orders WHERE `type`='CART' AND user_id='" . $UsID . "'";
-$reponseorder=$bdd->query($request);
-while($datareponse=$reponseorder->fetch()){
-	echo $datareponse['id'];
-	echo "\n";
-	$id=$datareponse['id'];
 
-}
-$request_amount="SELECT amount FROM `orders` WHERE id=$id";
-$reponse_request_amount=$bdd->query($request_amount);
-$amount=$reponse_request_amount->fetch();
-echo $amount['amount'];
-$prix=$amount['amount'];
-echo "\n";
-
-$request_order_product="SELECT product_id,quantity FROM `order_products` WHERE id=$id";
-$reponse_request_order_product=$bdd->query($request_order_product);
-$order_product=$reponse_request_order_product->fetch();
-$quantity=$order_product['quantity'];
-echo $quantity;
-echo "\n";
-
-$product_id=$order_product['product_id'];
-echo $product_id
-
-
-
-
-
-
-  ?>
 <html>
 <head>
-<header>
-	<link rel="stylesheet" href="css/head.css" />
-        <h1>P'ISENLOVE</h1>  	<!-- -->
-    </header>
-	<title>boulanger.com</title>
+    <link rel="stylesheet" href="style.css" />
+	<title>Capsules.com</title>
 	<b><font size="+3">Votre Panier:</font><b>
 	<u><h3>Articles dans votre panier(<?php echo $quantity ?>)<h3></u>
-
 </head>
+
 <body>
+    <?php include 'myHeaderLigne.php'?>  
+
+    <?php
+        $UsID=1;/*$_GET['user'];*/
+        $request="SELECT * FROM orders WHERE `type`='CART' AND user_id='" . $UsID . "'";
+        $reponseorder=$bdd->query($request);
+        while($datareponse=$reponseorder->fetch()){
+            echo $datareponse['id'];
+            echo "\n";
+            $id=$datareponse['id'];
+
+        }
+        $request_amount="SELECT amount FROM `orders` WHERE id=$id";
+        $reponse_request_amount=$bdd->query($request_amount);
+        $amount=$reponse_request_amount->fetch();
+        echo $amount['amount'];
+        $prix=$amount['amount'];
+        echo "\n";
+
+        $request_order_product="SELECT product_id,quantity FROM `order_products` WHERE id=$id";
+        $reponse_request_order_product=$bdd->query($request_order_product);
+        $order_product=$reponse_request_order_product->fetch();
+        $quantity=$order_product['quantity'];
+        echo $quantity;
+        echo "\n";
+
+        $product_id=$order_product['product_id'];
+        echo $product_id
+    ?>
+
 	<div id="paniervide">
 		<a href="https://www.google.fr/?gws_rd=ssl">
 	<div id=article1>
