e1e2bce1b583680dfa50cf1e0a04970688d11c75
noemieblt <noemieboillot5@hotmail.fr>
2018-10-15 16:49:22 +0200

    Update myHeaderLigne.php

--- myHeaderLigne.php
+++ myHeaderLigne.php
@@ -12,7 +12,7 @@
 
 <?php
 	
-	try	{			$bdd = new PDO('mysql:host=localhost;dbname=project;charset=utf8', 'root', '');		}
+	try	{			$bdd = new PDO('mysql:host=localhost;dbname=capsules;charset=utf8', 'root', '');		}
 	catch (Exception $e)	{	        die('Erreur : ' . $e->getMessage());		}
 	
 	$user_id = '';
