f20a0c00dc7dcc49d3cd3799a71eca4203175810
SamLef <43677207+SamLef@users.noreply.github.com>
2018-10-23 18:01:37 +0200

    correctifs divers

--- empty card.php
+++ empty card.php
@@ -9,28 +9,30 @@
 </head>  
 <h1>Votre Panier:</h1>
 <?php
-        $UsID=$_SESSION['id'];/*$_GET['user'];*/
-        $request="SELECT * FROM orders WHERE `type`='CART' AND user_id='" . $UsID . "'";
-        $reponseorder=$bdd->query($request);
-        while($datareponse=$reponseorder->fetch()){
-            $id=$datareponse['id'];
-        }
+    $UsID=$_SESSION['id'];/*$_GET['user'];*/
+    $request="SELECT * FROM orders WHERE `type`='CART' AND user_id='" . $UsID . "'";
+    $reponseorder=$bdd->query($request);
+    while($datareponse=$reponseorder->fetch()){
+        $id=$datareponse['id'];
+    }
 
-        if(empty($id))
-        {
-            echo "panier vide";
-        }
-        else
-        {
-            $request_amount="SELECT amount FROM `orders` WHERE id=$id";
-            $reponse_request_amount=$bdd->query($request_amount);
-            $amount=$reponse_request_amount->fetch();
-            $prix=$amount['amount'];
-            $reponse_request_amount->closeCursor();
-            echo "\n";
+    if(empty($id))
+    {
+        echo "panier vide";
+    }
+    else
+    {
+        $request_amount="SELECT amount FROM `orders` WHERE id=$id";
+        $reponse_request_amount=$bdd->query($request_amount);
+        $amount=$reponse_request_amount->fetch();
+        $prix=$amount['amount'];
+        $reponse_request_amount->closeCursor();
+        echo "\n";
 
-            $request_order_product="SELECT product_id,quantity,unit_price FROM `order_products` WHERE order_id = $id";
-            $reponse_request_order_product=$bdd->query($request_order_product);
+        $request_order_product="SELECT product_id,quantity,unit_price FROM `order_products` WHERE order_id = $id";
+        $reponse_request_order_product=$bdd->query($request_order_product);
+        ?>
+            <?php
             while($order_product = $reponse_request_order_product->fetch())
             {
                 $quantity=$order_product['quantity'];
@@ -43,37 +45,37 @@
                 $reponse_request_name_product->closeCursor();
                 ?>
                 <body>
-                <a href="product_descri.php?id=<?php echo $product_id; ?>">
-                <div id="monpanier">
-                    <div class="image_produit">
-                    <?php 
-                        echo '<img src="Images/'.$product_id.'"width="100%">';
-                    ?>
+                    <div id="produit_panier">
+                        <a href="product_descri.php?id=<?php echo $product_id; ?>">
+                            <div class="image_produit">
+                            <?php 
+                                echo '<img src="Images/'.$product_id.'"width="100%">';
+                            ?>
+                            </div>
+                            <div class="info_produit">
+                                <table>
+                                    <thead><tr>
+                                        <th>
+                                            <?php echo $name_product ?>
+                                        </th>
+                                    </tr></thead>
+                                    <tbody><tr>
+                                        <td>
+                                            Prix: <?php echo $prix_prod?>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            Quantité: <?php echo $quantity ?>
+                                        </td>
+                                    </tr></tbody>
+                                </table>    
+                            </div>
+                        </a>
                     </div>
-                    <div class="info_produit">
-                        <table>
-                            <thead><tr>
-                                <th>
-                                    <?php echo $name_product ?>
-                                </th>
-                            </tr></thead>
-                            <tbody><tr>
-                                <td>
-                                    Prix: <?php echo $prix_prod?>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    Quantité: <?php echo $quantity ?>
-                                </td>
-                            </tr></tbody>
-                        </table>    
-                    </div>
-                </div>
-                </a><br>
                 </body>
             <?php
             }
-            $reponse_request_order_product->closeCursor();
-        }
+        $reponse_request_order_product->closeCursor();
+    }
  ?>  
