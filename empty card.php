380c99982b6c3e92baf76ead13fd1482e3b7f2ab
SamLef <43677207+SamLef@users.noreply.github.com>
2018-10-25 14:31:20 +0200

    backup

---
+++ empty card.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+
+<html>
+
+<?php include 'myHeaderLigne.php'?>
+<head>
+    <link rel="stylesheet" href="style.css" />
+    <title>Capsules.com</title>
+</head>  
+<h1>Votre Panier:</h1>
+<?php
+    $UsID=$_SESSION['id'];/*$_GET['user'];*/
+    $request="SELECT * FROM orders WHERE `type`='CART' AND user_id='" . $UsID . "'";
+    $reponseorder=$bdd->query($request);
+    while($datareponse=$reponseorder->fetch()){
+        $id=$datareponse['id'];
+    }
+
+    if(empty($id))
+    {
+        echo "panier vide";
+    }
+    else
+    {
+        $request_amount="SELECT amount FROM `orders` WHERE id=$id";
+        $reponse_request_amount=$bdd->query($request_amount);
+        $amount=$reponse_request_amount->fetch();
+        $prix=$amount['amount'];
+        $reponse_request_amount->closeCursor();
+        echo "\n";
+
+        $request_order_product="SELECT product_id,quantity,unit_price FROM `order_products` WHERE order_id = $id";
+        $reponse_request_order_product=$bdd->query($request_order_product);
+        ?>
+            <?php
+            while($order_product = $reponse_request_order_product->fetch())
+            {
+                $quantity=$order_product['quantity'];
+                $product_id=$order_product['product_id'];
+                $prix_prod=$order_product['unit_price'];
+                $request_name_product="SELECT name FROM products WHERE id =$product_id";
+                $reponse_request_name_product=$bdd->query($request_name_product);
+                $rep_name= $reponse_request_name_product->fetch();
+                $name_product=$rep_name['name'];
+                $reponse_request_name_product->closeCursor();
+                ?>
+                <body>
+                    <div id="produit_panier">
+                        <a href="product_descri.php?id=<?php echo $product_id; ?>">
+                            <div class="image_produit">
+                            <?php 
+                                echo '<img src="Images/'.$product_id.'"width="100%">';
+                            ?>
+                            </div>
+                            <div class="info_produit">
+                                <table>
+                                    <thead><tr>
+                                        <th>
+                                            <?php echo $name_product ?>
+                                        </th>
+                                    </tr></thead>
+                                    <tbody><tr>
+                                        <td>
+                                            Prix: <?php echo $prix_prod?>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            Quantité: <?php echo $quantity ?>
+                                        </td>
+                                    </tr></tbody>
+                                </table>    
+                            </div>
+                        </a>
+                    </div>
+                </body>
+            <?php
+            }
+        $reponse_request_order_product->closeCursor();
+    }
+ ?>  
---
+++ myHeaderLigne.php
@@ -0,0 +1,101 @@
+
+<?php
+	
+	try	{			$bdd = new PDO('mysql:host=localhost;dbname=capsules;charset=utf8', 'root', '');		}
+	catch (Exception $e)	{	        die('Erreur : ' . $e->getMessage());		}
+	
+	$user_id = '';
+	$co=false;
+
+	session_start ();
+	
+	if (isset($_POST['deco']) ){
+    		session_unset ();
+			session_destroy ();
+#	   		echo 'vous venez de vous déconnecter';  
+    }
+
+	if (isset($_SESSION['identifiant']) && isset($_SESSION['password'])) {
+#		echo "Vous etes connecté </br>";
+		$co=true;	
+	}
+
+	if(isset($_POST['identifiant']) AND isset($_POST['mdp'])  ){
+    	$identifiant = $_POST['identifiant'] ;
+    	$mdp = $_POST['mdp'] ;
+    	$result = $bdd->query('SELECT * FROM users');
+    	$donnee = $result->fetch();
+    	while($donnee != null){
+    		if($donnee['username']==$identifiant AND $donnee['password']==$mdp){
+    			$user_id = $donnee['id'];
+				$_SESSION['identifiant'] = $identifiant;
+				$_SESSION['password'] = $mdp;
+				$_SESSION['id'] = $user_id;
+#    			echo 'vous venez de vous connecter';
+				$co=true;
+    		}
+    		$donnee = $result->fetch();
+    	} 
+    	$result->closeCursor();
+    }
+
+
+?>
+
+
+<!DOCTYPE html>
+
+<html>
+<head><link rel="stylesheet" href="style.css" /></head>
+
+
+<header>
+
+
+
+<div id="conteneur1">
+
+	<div class="element3">	<img src="images/macaps.png" width="50px">	</div>
+
+	<div class="element1">  <a id="nomdusite" href="myHeaderLigne.php">Ma Capsule</a> </div>
+
+	<div class="element2" >
+
+<?php 
+	if ($co==false){
+		echo '
+		<form id="connexion" method="post"> 
+			Username : <input type="text" name="identifiant" />
+			<input type="submit" value="Envoyer" />
+			</br>
+			Password : <input type="password" name="mdp"/> 
+			</form>
+
+		' ;
+	}
+		
+	else {
+		echo '
+		<form id="deconnexion" method="post">
+		    <div id="hide"> <input type="text" name="deco" value="decon"/> </div>
+	    	<input type="submit" value="Me déconnecter" />
+		</form>
+		';
+	}
+?>
+
+	</div>
+
+</div>
+
+<div id="conteneur2">
+    
+    <div class="elem1"> <a id="produits" href="search_p.php">LesProduits</a> </div>
+    <div class="elem3"> <a id="panier" href="empty card.php">MonPanier</a> </div>
+
+</div>
+	
+
+</header>
+
+</html>
