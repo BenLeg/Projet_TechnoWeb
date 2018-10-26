b4e48046d8c55230c2a981bd8a73a6a7bf4d476f
noemieblt <noemieboillot5@hotmail.fr>
2018-10-26 15:07:01 +0200

    Update myHeaderLigne.php

--- myHeaderLigne.php
+++ myHeaderLigne.php
@@ -91,7 +91,7 @@
 <div id="conteneur2">
     
     <div class="elem1"> <a id="produits" href="liste_produits.php">LesProduits</a> </div>
-    <div class="elem2"><input type="text" href="search_l.php" value='Search'></div>	
+    <div class="elem2"><input type="text" href="search.php" value='Search'></div>	
 <?php 
 	if ($co==true){
 		echo '  <div class="elem3"> <a id="panier" href="empty card.php">MonPanier</a> </div> ';
