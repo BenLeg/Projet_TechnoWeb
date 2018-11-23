aa8b731ed58fd8e49d27ebd0c177babc12f0bff9
noemieblt <noemieboillot5@hotmail.fr>
2018-11-23 21:39:27 +0100

    Update search.php

--- views/search.php
+++ views/search.php
@@ -9,10 +9,9 @@ $recherche = $_POST['recherche'];
    <body>
 
 		<section>
-            <h1>Résultat de la recherche</h1>
-            <br>
-            <div id="conteneur_principal">
+            
             <?php
+            $trouve = 0;
             $reponse = $bdd->query('SELECT * FROM products');
             $traitement_en_cours=true;
             while($traitement_en_cours)
@@ -24,12 +23,19 @@ $recherche = $_POST['recherche'];
                     break;
                 }
                 if( strcasecmp($donnees['name'], $recherche) == 0){
-                	?>
+                	
 				
+                if ($trouve == 0) { 
+                    echo '
+                    <h1>Résultat de la recherche</h1>
+                        <br>
+                        <div id="conteneur_principal">    ';
+            } ?>
                     <div class="element">
                         <a href=index.php?page=fiche_produit&id=<?php echo $donnees['id']; ?>">
                             <figure>
                                 <?php 
+                                    $trouve = 1;
                                     $image=$donnees['id'];
                                     echo '<img src="Images/'.$image.'" width="100%">';
                                 ?>
@@ -48,6 +54,16 @@ $recherche = $_POST['recherche'];
             <?php             }       
             }
             $reponse->closeCursor();
+
+
+            if($trouve == 0){
+                echo '
+                </br></br>
+                    Aucun article ne correspond à votre
+                </br></br></br>
+                ';
+            }
+
             ?>
             </div>
     	</section>
